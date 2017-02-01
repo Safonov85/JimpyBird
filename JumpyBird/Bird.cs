e39ca7a54bf5aa27cf4773cb6df458463a090600
Safonov85 <safonov85@yahoo.com>
2017-02-01 20:24:30 +0100

    Bird can Jump with SPACE

--- JumpyBird/Bird.cs
+++ JumpyBird/Bird.cs
@@ -10,13 +10,24 @@ namespace JumpyBird
     class Bird
     {
         public Rectangle bird;
+        public SolidBrush birdBrush;
         public int xWidth, yHeight;
         public DateTime time;
 
         public Bird(int xPosition, int yPosition)
         {
+            xWidth = 20;
+            yHeight = 20;
             bird = new Rectangle(xPosition, yPosition, xWidth, yHeight);
             time = DateTime.Now;
+            birdBrush = new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(220, 190, 150));
+        }
+
+        public int SimpleFall(int yPosition)
+        {
+            yPosition += 2;
+
+            return yPosition;
         }
 
         public void Gravity(int yPosition)
--- JumpyBird/JumpyBird.cs
+++ JumpyBird/JumpyBird.cs
@@ -16,12 +16,17 @@ namespace JumpyBird
         SolidBrush brush;
         SolidBrush wallBrush;
         Color backgroundColor = Color.FromArgb(70, 70, 70);
-        int xPosition, yPosition, xPositionDown, yPositionDown;
+        int yPosition, xPositionDown, yPositionDown;
+        int[] xPosition;
         int xBirdPosition, yBirdPosition;
+        int newWall;
+        int jumpStop;
+        bool spacePress;
         Graphics graphics;
         Graphics graphicsOverride;
 
         List<Wall> wall;
+        Bird bird;
 
         public JumpyBird()
         {
@@ -33,14 +38,38 @@ namespace JumpyBird
         {
             graphics.Clear(backgroundColor);
             //graphics.FillRectangle(brush, new Rectangle(100, 100, 100, 100));
-
+            int itemCount = 0;
             foreach(Wall item in wall)
             {
-                item.wallUp = new Rectangle(xPosition, yPosition, item.xWidth, item.yHeight);
+                item.wallUp = new Rectangle(xPosition[itemCount], yPosition, item.xWidth, item.yHeight);
                 graphics.FillRectangle(item.wallColor, item.wallUp);
+                itemCount++;
             }
 
-            xPosition -= 2;
+            bird = new Bird(xBirdPosition, yBirdPosition);
+            graphics.FillRectangle(bird.birdBrush, bird.bird);
+            itemCount = 0;
+            while(itemCount < 4)
+            {
+                xPosition[itemCount] -= 2;
+                itemCount++;
+            }
+            newWall += 2;
+
+            if(spacePress == false)
+            {
+                yBirdPosition = bird.SimpleFall(yBirdPosition);
+            }
+            else
+            {
+                yBirdPosition -= 3;
+                jumpStop += 3;
+                if(jumpStop > 40)
+                {
+                    spacePress = false;
+                    jumpStop = 0;
+                }
+            }
 
             graphicsOverride.DrawImage(bitmap, 0, 0, this.Width, this.Height);
         }
@@ -52,15 +81,35 @@ namespace JumpyBird
             bitmap = new Bitmap(this.Width, this.Height);
             graphics = Graphics.FromImage(bitmap);
             graphicsOverride = this.CreateGraphics();
-            xPosition = 370;
             wall = new List<Wall>();
+            xBirdPosition = 50;
+            yBirdPosition = 200;
+            bird = new Bird(xBirdPosition, yBirdPosition);
+            xPosition = new int[4];
+            xPosition[0] = 370;
+            newWall = 0;
+            jumpStop = 0;
+            spacePress = false;
             AddWall();
             timer.Start();
         }
 
         void AddWall()
         {
-            wall.Add(new Wall(xPosition, yPosition, xPositionDown, yPositionDown));
+            if(newWall < 250)
+            {
+                wall.Add(new Wall(xPosition[0], yPosition, xPositionDown, yPositionDown));
+                newWall = 0;
+            }
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if(keyData == Keys.Space)
+            {
+                spacePress = true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
         }
     }
 }
